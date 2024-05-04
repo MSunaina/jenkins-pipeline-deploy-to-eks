804675dfb25694ec849ab480f7272e213f49036e
MSunaina <mudumbaisunaina@gmail.com>
2024-05-04 18:09:52 +0530

    added files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.19.0"
@@ -26,3 +25,31 @@ module "myapp-vpc" {
     "kubernetes.io/role/internal-elb"         = 1
   }
 }
+
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "myapp-eks-cluster"
+    cluster_version = "1.24"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.myapp-vpc.vpc_id
+    subnet_ids = module.myapp-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "myapp"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.small"]
+        }
+    }
+}
+
