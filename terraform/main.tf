f2d9227937a3419ae756c66dfdbf11762fe21bbb
MSunaina <54520593+MSunaina@users.noreply.github.com>
2024-05-04 18:20:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,54 @@
-module "myapp-vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
-  name            = "myapp-vpc"
-  cidr            = var.vpc_cidr_block
-  private_subnets = var.private_subnet_cidr_blocks
-  public_subnets  = var.public_subnet_cidr_blocks
-  azs             = data.aws_availability_zones.azs.names
+#Vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "eks_cluster_vpc"
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.azs.names
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+
+  enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
-  enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
   }
-
   public_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = 1
-  }
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
 
+  }
   private_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/private_elb"       = 1
+
   }
 }
+
+#EKS
 
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
 
-    cluster_endpoint_public_access  = true
-
-    vpc_id = module.myapp-vpc.vpc_id
-    subnet_ids = module.myapp-vpc.private_subnets
-
-    tags = {
-        environment = "development"
-        application = "myapp"
+  eks_managed_node_groups = {
+    nodes = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instance_types
     }
-
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 3
-            desired_size = 2
-
-            instance_types = ["t2.small"]
-        }
-    }
+  }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
-
