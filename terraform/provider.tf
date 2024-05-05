bd8a1ddf14b60b61d983b3b4164b069b8317b383
MSunaina <mudumbaisunaina@gmail.com>
2024-05-05 12:55:30 +0530

    files modified

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,13 @@
-provider "aws" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
 
-  region = "us-east-1"
-
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
 }
